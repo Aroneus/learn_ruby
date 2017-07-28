b2c0587e1728b0ed371acec0a74549243e22c845
Aaron Pietsch <apietsch4@gmail.com>
2017-07-27 15:07:46 -1000

    finished all challenges, not the most streamlined but I'm learning

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+attr_reader :title
+
+  def title=(input)
+      argv = input.split.map.with_index do |x, i|
+          if x =~ /[0-9]/
+            x
+          elsif i == 0
+            x.capitalize!
+          elsif x == "i"
+            x.capitalize!
+          elsif %w(the a an and in of).include?(x)
+            x
+          else
+            x.capitalize!
+          end
+        end
+      @title = argv.join(" ")
+    end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,48 @@
 class Timer
-  #write your code here
-end
+attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours = @seconds / 3600
+
+    if hours == 0
+      minutes = @seconds / 60
+    else
+      minutes = (@seconds - hours * 3600) / 60
+    end
+
+    if hours >= 1
+      @seconds = @seconds - (hours * 3600)
+    else
+      hours = 0
+    end
+
+    if minutes >= 1
+      @seconds = @seconds - (minutes * 60)
+    else
+      minutes = 0
+    end
+
+    time = "#{hours} #{minutes} #{@seconds}"
+
+    timer = time.split.map do |x|
+      if x.length == 0
+        x = "00"
+      elsif x.length == 1
+        x = "0#{x}"
+      else
+        x
+      end
+    end
+
+    @time_string = timer.join(":")
+
+    end
+    @time_string
+  end
+
+
+@timer = Timer.new
+puts @timer.seconds = 10
